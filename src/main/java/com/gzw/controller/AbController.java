82c67850726b52c70481a1a1a7891bc97933bd22
Zachary <954979487@qq.com>
2017-11-08 16:27:39 +0800

    Initial commit

---
+++ src/main/java/com/gzw/controller/AbController.java
@@ -0,0 +1,8 @@
+package com.gzw.controller;
+
+/**
+ * @author:龚增伟
+ * @Date :2017/11/7 17:16
+ */
+public class AbController {
+}
---
+++ src/main/java/com/gzw/controller/UserMainController.java
@@ -0,0 +1,31 @@
+package com.gzw.controller;
+/**
+ * @author:龚增伟
+ * @Date :2017/11/7 17:16
+ */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+@RequestMapping("/user")
+public class UserMainController {
+
+
+    /**
+    * @author: 龚增伟
+    * @describe: TODO
+    * param:  * @param null
+    * @Date: 2017/11/7 17:44
+    **/
+    @RequestMapping("/login")
+    public String login(){
+        System.out.println("hello idea");
+        return "web/index";
+    }
+
+
+
+}
