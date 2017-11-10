1ecd941b26b21a980709d56e74567f550aac793a
Zachary <954979487@qq.com>
2017-11-10 11:25:01 +0800

    删除了2个无用的测试方法

--- src/main/java/com/gzw/controller/AbController.java
+++
@@ -1,8 +0,0 @@
-package com.gzw.controller;
-
-/**
- * @author:龚增伟
- * @Date :2017/11/7 17:16
- */
-public class AbController {
-}
--- src/main/java/com/gzw/controller/Test.java
+++
@@ -1,8 +0,0 @@
-package com.gzw.controller;
-
-/**
- * @author:龚增伟
- * @Date :2017/11/7 17:17
- */
-public class Test {
-}
